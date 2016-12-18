647aea9556c1c7893a8eaa9cd634c2a52d895b25
Emin Tham <emin@securitycompass.com>
2016-12-18 14:04:14 -0500

    Script to calculate probabilities of tiles
    - also git ignore the tiles.txt file.

---
+++ calculate_probabilities.py
@@ -0,0 +1,26 @@
+from collections import defaultdict
+
+
+def read_from_file(filename='tiles.txt'):
+    tiles = defaultdict(lambda: defaultdict(int))
+
+    with open(filename, 'rb') as f:
+        for line in f:
+            tile, score = [int(i) for i in line.split(' ')]
+            score_category = score / 100
+            tiles[score_category][tile] += 1
+
+    return tiles
+
+
+def print_probabilities(d):
+    for score_category, dictionary in d.iteritems():
+        total = float(sum(dictionary.values()))
+        print 'score_category: {}'.format(score_category)
+        for tile, number in dictionary.iteritems():
+            print '{}: {:.3f}'.format(tile, number/total)
+
+
+if __name__ == '__main__':
+    d = read_from_file()
+    print_probabilities(d)
