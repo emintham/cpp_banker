09978d3ed331957a5a35a8a4cfbca3313c68c95f
Emin Tham <emin@securitycompass.com>
2016-12-24 16:56:09 -0500

    Minor formatting changes

--- board.h
+++ board.h
@@ -15,50 +15,49 @@ class Board;
 typedef std::shared_ptr<Board> BoardPtr;
 
 class Board {
+  public:
+    std::array<Tile, BOARD_SIZE> board = {Tile(0), Tile(0), Tile(0), Tile(0), Tile(0),
+                                          Tile(0), Tile(0), Tile(0), Tile(0), Tile(0),
+                                          Tile(0), Tile(0), Tile(1), Tile(0), Tile(0),
+                                          Tile(0), Tile(0), Tile(0), Tile(0), Tile(0),
+                                          Tile(0), Tile(0), Tile(0), Tile(0), Tile(0)};
+    int competitorTimers[BOARD_SIZE] = {0};
+    int bonus[BOARD_SIZE] = {0};
+    int score = 10;
+    int cash = 10;
 
-public:
-  std::array<Tile, BOARD_SIZE> board = {Tile(0), Tile(0), Tile(0), Tile(0), Tile(0),
-                                        Tile(0), Tile(0), Tile(0), Tile(0), Tile(0),
-                                        Tile(0), Tile(0), Tile(1), Tile(0), Tile(0),
-                                        Tile(0), Tile(0), Tile(0), Tile(0), Tile(0),
-                                        Tile(0), Tile(0), Tile(0), Tile(0), Tile(0)};
-  int competitorTimers[BOARD_SIZE] = {0};
-  int bonus[BOARD_SIZE] = {0};
-  int score = 10;
-  int cash = 10;
+    // ----- Methods ----------
+    // Util methods
+    void printCompetitorTimers() const;
+    bool isEmpty(int position) const;
+    bool isLawsuit(int position) const;
+    bool isPosLawsuit(int position) const;
+    bool isNegLawsuit(int position) const;
+    bool isNonProfit(int position) const;
+    bool isCompetitor(int position) const;
+    int numCompetitors() const;
+    bool isBankrupt() const;
+    int competitorCosts() const;
+    std::vector<std::tuple<int, int, int>> getMoveset() const;
 
-  // ----- Methods ----------
-  // Util methods
-  void printCompetitorTimers() const;
-  bool isEmpty(int position) const;
-  bool isLawsuit(int position) const;
-  bool isPosLawsuit(int position) const;
-  bool isNegLawsuit(int position) const;
-  bool isNonProfit(int position) const;
-  bool isCompetitor(int position) const;
-  int numCompetitors() const;
-  bool isBankrupt() const;
-  int competitorCosts() const;
-  std::vector<std::tuple<int, int, int>> getMoveset() const;
+    static void printMove(const int source, const int dest);
+    static const Tile getRandomTile(int score);
 
-  static void printMove(const int source, const int dest);
-  static const Tile getRandomTile(int score);
+    // Board modifying methods
+    void addCompetitor(int pos, Tile tile);
+    void clearCompetitor(int pos);
+    void addBonus(int pos, int value);
+    BoardPtr move(const int source, const int dest, const Tile& nextTile);
 
-  // Board modifying methods
-  void addCompetitor(int pos, Tile tile);
-  void clearCompetitor(int pos);
-  void addBonus(int pos, int value);
-  BoardPtr move(const int source, const int dest, const Tile& nextTile);
+    friend std::ostream& operator<<(std::ostream& os, const Board b);
 
-  friend std::ostream& operator<<(std::ostream& os, const Board b);
+  private:
+    int competitors = 0;
 
-private:
-  int competitors = 0;
-
-  void updateTimer();
-  void updateBonus();
-  BoardPtr walk(const int source, const int dest, const Tile& nextTile) const;
-  BoardPtr jump(const int source, const int dest, const Tile& nextTile, const int start, const int dist, const bool horizontalJump) const;
+    void updateTimer();
+    void updateBonus();
+    BoardPtr walk(const int source, const int dest, const Tile& nextTile) const;
+    BoardPtr jump(const int source, const int dest, const Tile& nextTile, const int start, const int dist, const bool horizontalJump) const;
 };
 
 #endif
--- emm.h
+++ emm.h
@@ -4,25 +4,25 @@
 #include "board.h"
 
 class EMM {
-  static bool markReuse;
-  static int reusedValues;
+    static bool markReuse;
+    static int reusedValues;
 
-public:
-  static void rollout();
-  static int rolloutOnce(int depth);
-  static void commandParser();
-  static BoardPtr solveBestMove(const BoardPtr& b, const Tile& nextTile, int depth, int *dist);
-  static BoardPtr solveBestMove(const BoardPtr& b, const Tile& nextTile, int depth, int *dist, bool verbose);
-  static BoardPtr handleLawsuit(std::istringstream& currentLine, std::ofstream& tileFile, const BoardPtr& b, const int depth);
-  static BoardPtr handleBonus(std::istringstream& currentLine, std::ofstream& tileFile, const BoardPtr& b, const int depth);
-  static BoardPtr handleNonProfit(std::istringstream& currentLine, std::ofstream& tileFile, const BoardPtr& b, const int depth);
-  static void handleDebug(std::istringstream& currentLine, std::ofstream& tileFile, const BoardPtr& b);
-  static BoardPtr handleTile(const int nextTile, std::ofstream& tileFile, const BoardPtr& b, const int depth);
+  public:
+    static void rollout();
+    static int rolloutOnce(int depth);
+    static void commandParser();
+    static BoardPtr solveBestMove(const BoardPtr& b, const Tile& nextTile, int depth, int *dist);
+    static BoardPtr solveBestMove(const BoardPtr& b, const Tile& nextTile, int depth, int *dist, bool verbose);
+    static BoardPtr handleLawsuit(std::istringstream& currentLine, std::ofstream& tileFile, const BoardPtr& b, const int depth);
+    static BoardPtr handleBonus(std::istringstream& currentLine, std::ofstream& tileFile, const BoardPtr& b, const int depth);
+    static BoardPtr handleNonProfit(std::istringstream& currentLine, std::ofstream& tileFile, const BoardPtr& b, const int depth);
+    static void handleDebug(std::istringstream& currentLine, std::ofstream& tileFile, const BoardPtr& b);
+    static BoardPtr handleTile(const int nextTile, std::ofstream& tileFile, const BoardPtr& b, const int depth);
 
-private:
-  static int heuristicScore(const BoardPtr& b);
-  static float bestMove(const BoardPtr& b, const Tile& nextTile, int depth, int* source, int* dest);
-  static float expectiminimax(const BoardPtr& board, int depth);
+  private:
+    static int heuristicScore(const BoardPtr& b);
+    static float bestMove(const BoardPtr& b, const Tile& nextTile, int depth, int* source, int* dest);
+    static float expectiminimax(const BoardPtr& board, int depth);
 };
 
 #endif
