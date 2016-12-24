b582169014e254c4c07a138f263aa17e84a5caff
Emin Tham <emin@securitycompass.com>
2016-12-23 23:07:45 -0500

    Make python calculation print nicer

--- calculate_probabilities.py
+++ calculate_probabilities.py
@@ -10,19 +10,21 @@ def read_from_file(filename='tiles.txt'):
             if tile.startswith('$'):
                 continue
 
-            score = int(score)
-            score_category = score / 100
+            score_category = int(score) / 100
             tiles[score_category][tile] += 1
 
     return tiles
 
 
 def print_probabilities(d):
+    tile_types = ['2', '1', '0', '-1', '-2', '.2', '.3', '-', '+']
+
     for score_category, dictionary in d.iteritems():
         total = float(sum(dictionary.values()))
-        print 'score_category: {}'.format(score_category)
-        for tile, number in dictionary.iteritems():
-            print '{}: {:.3f}'.format(tile, number/total)
+        print 'score < {}:'.format((score_category + 1) * 100)
+        for tile_type in tile_types:
+            number = dictionary.get(tile_type, 0)
+            print '{}: {:.3f}'.format(tile_type, number/total)
 
 
 if __name__ == '__main__':
